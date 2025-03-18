1666abddda15309097bf9877bde4baaaceec26fc
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2025-03-18 14:09:21 +0100

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod network;
+mod player;
+
+use std::io;
+use std::sync::{Arc, Mutex};
+use player::{list_songs, play_song};
+use network::InternetConnection;
+
+fn main() {
+    let internet = Arc::new(Mutex::new(InternetConnection::new()));
+
+    loop {
+        println!("      Musikstreaming App");
+        println!("1. Afspil en sang");
+        println!("2. Toggle internetforbindelse");
+        println!("3. Afslut");
+
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice);
+
+        match choice.trim() {
+            "1" => {
+                list_songs();
+                println!("Indtast nummeret på sangen:");
+                let mut song_choice = String::new();
+                io::stdin().read_line(&mut song_choice);
+
+                if let Ok(song_index) = song_choice.trim().parse::<usize>() {
+                    play_song(song_index, Arc::clone(&internet));
+                } else {
+                    println!("Ugyldigt nummer");
+                }
+            }
+            "2" => { internet.lock().unwrap().toggle(); }
+            "3" => { break; }
+            _ => {}
+        }
+    }
+}
